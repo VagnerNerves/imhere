9f560b5baa9302e721c08f7fab0ff5a0ce78d077
Vagner Nerves <vagnernervessantos@gmail.com>
2023-01-03 19:16:55 -0300

    feat: Applied StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,32 +1,28 @@
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#131016',
-        padding: 24
-      }}
-    >
-      <Text
-        style={{
-          color: '#fff',
-          fontSize: 24,
-          fontWeight: 'bold',
-          marginTop: 48
-        }}
-      >
-        Nome do Evento
-      </Text>
-      <Text
-        style={{
-          color: '#6b6b6b',
-          fontSize: 16
-        }}
-      >
-        Sexta, 4 de Novembro 2022
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro 2022</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24
+  },
+  eventName: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: '#6b6b6b',
+    fontSize: 16
+  }
+})
