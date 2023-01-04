8625fb0b3e4395b4231df07435b679cfdf755ad1
Vagner Nerves <vagnernervessantos@gmail.com>
2023-01-04 20:10:03 -0300

    feat: Created Input and Button

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
     padding: 24
   },
   eventName: {
-    color: '#fff',
+    color: '#FFF',
     fontSize: 24,
     fontWeight: 'bold',
     marginTop: 48
@@ -15,5 +15,25 @@ export const styles = StyleSheet.create({
   eventDate: {
     color: '#6b6b6b',
     fontSize: 16
+  },
+  input: {
+    height: 56,
+    backgroundColor: '#1f1E25',
+    borderRadius: 5,
+    color: '#FFF',
+    padding: 16,
+    fontSize: 16
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 24
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
